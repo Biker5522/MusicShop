5ae446c7f30528c7cca1fb99f08dca36f4c39755
Kadath <dawidcichy34@gmail.com>
2022-01-12 22:43:09 +0100

    Add project files.

---
+++ MusicShop/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicShop.Models
+{
+    public class Album
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }  
+        public string Band { get; set;}
+        public string Genre { get; set; }
+        public int Year { get; set; }
+    }
+}
